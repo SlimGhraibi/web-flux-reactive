dd222da1c57d8893e1f3a50bff7a6d6a98e0057c
SlimGhraibi <slim.ghraibi1991@gmail.com>
2021-01-03 18:33:18 +0100

    test reactive save() deleteAll()

--- src/main/java/org/sid/WebFluxReactiveApplication.java
+++ src/main/java/org/sid/WebFluxReactiveApplication.java
@@ -3,11 +3,13 @@ package org.sid;
 import org.sid.dao.SocieteRepository;
 import org.sid.dao.TransactionRepository;
 import org.sid.entities.Societe;
+import org.sid.entities.Transaction;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.Instant;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -21,10 +23,25 @@ public class WebFluxReactiveApplication {
     @Bean
     CommandLineRunner start(SocieteRepository societeRepository, TransactionRepository transactionRepository) {
         return args -> {
-            societeRepository.deleteAll().subscribe(null, null, ()-> {
+            societeRepository.deleteAll().subscribe(null, null, () -> {
                 Stream.of("A", "B", "C", "D").forEach(bank -> {
                     societeRepository.save(new Societe(bank, bank, 100 + Math.random() * 900)).subscribe(societe -> {
                         System.out.println(societe.toString());
+                    });
+                });
+                transactionRepository.deleteAll().subscribe(null, null, ()-> {
+                    Stream.of("A", "B", "C", "D").forEach(bank -> {
+                        societeRepository.findById(bank).subscribe(soc -> {
+                            for (int i = 0; i < 10; i++) {
+                                Transaction transaction = new Transaction();
+                                transaction.setInstant(Instant.now());
+                                transaction.setPrice(soc.getPrice() * (1 + (Math.random() * 12 - 6) / 100));
+                                transaction.setSociete(soc);
+                                transactionRepository.save(transaction).subscribe(t -> {
+                                    System.out.println("transaction => " + t.toString());
+                                });
+                            }
+                        });
                     });
                 });
             });
